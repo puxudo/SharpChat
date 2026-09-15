041043d716f3f590a5d506c621c2ea858f020b2f
puxudo <mymainonproton@proton.me>
2026-09-15 11:15:28 +0330

    fixed a minor bug

--- SharpChat.UI/src/Icons.jsx
+++ SharpChat.UI/src/Icons.jsx
@@ -115,14 +115,6 @@ export function IconFile(props) {
         </svg>
     );
 }
-export function IconPaperclip(props) {
-    return (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" {...props}>
-            <path d="M21.44 11.05l-9.19 9.19a5 5 0 01-7.07-7.07l9.19-9.19a3.5 3.5 0 014.95 4.95l-9.2 9.19a2 2 0 01-2.83-2.83l8.49-8.48" />
-        </svg>
-    );
-}
-
 export function IconSmile(props) {
     return (
         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" {...props}>
